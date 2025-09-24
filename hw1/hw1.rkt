08e86529836c0880e5c01ad9550299dc10d74054
JasonCourtois <148589763+JasonCourtois@users.noreply.github.com>
2025-09-24 10:26:35 -0400

    HW 1 upload

--- hw1/hw1.rkt
+++ hw1/hw1.rkt
@@ -27,42 +27,170 @@
 ;; otherwise the grader will not work and your submission will get 0 points.
 (provide (all-defined-out))
 
-(define ex1 'todo)
-(define ex2 'todo)
-(define ex3 'todo)
+;; Original Equation was ((13−1)+10)−(6+(5+11))
+(define ex1 
+  (- 
+    (+ 
+      (- 13 1) 
+      10) 
+    (+ 
+      6 
+      (+ 5 11))))
+
+(define ex2 
+  (list 
+    ;; Initial Equation
+    (- 
+      (+ 
+        (- 13 1) 
+        10) 
+      (+ 
+        6 
+        (+ 5 11)))
+    ;; Compute 13 - 1
+    (- 
+      (+ 12 10) 
+      (+ 
+        6 
+        (+ 5 11)))
+    ;; Compute 12 + 10
+    (- 
+      22 
+      (+ 
+        6 
+        (+ 5 11)))
+    ;; Compute 5 + 11
+    (- 
+      22 
+      (+ 6 16))
+    ;; Compute 6 + 16
+    (- 22 22)
+    ;; Compute 22 - 22
+    0))
+
+;; Original Function
+;; def ex3(x, y):
+;;     return (x * 4) - x == (y - 6) + (x - 6)
+(define
+  (ex3 x y) 
+    ;; Function takes x and y input, and computes (x * 4) - x == (y - 6) + (x - 6)
+    (= 
+      (- 
+        (* x 4) 
+        x) 
+      (+ 
+        (- y 6) 
+        (- x 6))))
+
+;; EXERCISE 4
 
 ;; Constructs a tree from two trees and a value
-(define (tree left value right) 'todo)
-;; Constructs a tree with a single node
-(define (tree-leaf value) 'todo)
+(define (tree left value right) (list left value right))
+;; Constructs a tree no children
+(define (tree-leaf value) (list null value null))
 
 ;; Accessors
-(define (tree-left self) 'todo)
-(define (tree-value self) 'todo)
-(define (tree-right self) 'todo)
+(define (tree-left self) (first self))
+(define (tree-value self) (second self))
+(define (tree-right self) (third self))
 
 ;; Copies the source and updates one of the fields
-(define (tree-set-value self value) 'todo)
-(define (tree-set-left self left) 'todo)
-(define (tree-set-right self right) 'todo)
+(define (tree-set-value self value) 
+  (tree (tree-left self) value (tree-right self)))
+
+(define (tree-set-left self left) 
+  (tree left (tree-value self) (tree-right self)))
+
+(define (tree-set-right self right) 
+  (tree (tree-left self) (tree-value self) right))
 
 ;; Function that inserts a value in a BST
-(define (bst-insert self value) 'todo)
+(define (bst-insert self value)
+  ;; The equivalent Python code is above each line for reference on how syntax was chosen.
+  (cond 
+    ;; if node is None:
+    ;;  return Tree(None, value, None) - This is equivalent to our tree-leaf constructor.
+    [(null? self) 
+      (tree-leaf value)]
+    
+    ;; if value == node.value:
+    ;;  return node.set_value(value)
+    [(= value (tree-value self)) 
+      (tree-set-value self value)]
+    
+    ;; if value < node.value:
+    ;;  return node.set_left(insert(node.left, value))
+    [(< value (tree-value self)) 
+      (tree-set-left self (bst-insert (tree-left self) value))]
+    
+    ;; return node.set_right(insert(node.right, value)) 
+    [else 
+      (tree-set-right self (bst-insert (tree-right self) value))]))
+
+;; EXERCISE 5
 
 ;; lambda
-(define (lambda? node) 'todo)
-(define (lambda-params node) 'todo)
-(define (lambda-body node) 'todo)
+(define (lambda? node) 
+  (and
+      ;; Check if the input is a list.
+      (list? node)
+      ;; Lambda definition will have at least 3 items.
+      (>= (length node) 3)
+      ;; The first item should always be the symbol 'lambda.
+      (equal? 'lambda (first node))
+      ;; The parameters should be a list of symbols.
+      (list? (lambda-params node))
+      (andmap symbol? (lambda-params node))
+      ;; There should be at least one item in the body
+      (>= (length (lambda-body node)) 1)))
+
+(define (lambda-params node) (second node)) ;; List of params in lambda def is the second item
+(define (lambda-body node) (rest (rest node))) ;; The remaining body of the lambda is after the first two items.
 
 ;; apply
-(define (apply? l) 'todo)
-(define (apply-func node) 'todo)
-(define (apply-args node) 'todo)
+(define (apply? l) 
+  (and 
+    ;; Every function application should be a list that is at least 1 item long.
+    (list? l)
+    (>= (length l) 1)
+    ;; The first item in the function should follow lambda syntax or be a symbol.
+    (or 
+      (lambda? (apply-func l))
+      (symbol? (apply-func l)))))
+
+(define (apply-func node) (first node)) ;; Returns symbol of function being called
+(define (apply-args node) (rest node))  ;; Returns list of arguments passed into function.
 
 ;; define
-(define (define? node) 'todo)
-(define (define-basic? node) 'todo)
-(define (define-func? node) 'todo)
+(define (define? node) 
+  ;; All defines will either be a basic or function definition.
+  (or 
+    (define-basic? node)
+    (define-func? node)))
+
+(define (define-basic? node) 
+  (and
+    ;; Check if the input is a list
+    (list? node)
+    ;; A basic definition will always have 3 items.
+    (equal? (length node) 3)
+    ;; The first item should always be the symbol 'define
+    (equal? 'define (first node))
+    ;; The second item should be any symbol for the name of the variable.
+    (symbol? (second node))))
+
+(define (define-func? node) 
+  (and
+      ;; Check if the input is a list
+      (list? node)
+      ;; A function definition will always have at least 3 items.
+      (>= (length node) 3)
+      ;; The first item should always be the symbol 'define
+      (equal? 'define (first node))
+      ;; The second item should be a list with at least one item, which should all be symbols.
+      (list? (second node))
+      (>= (length (second node)) 1)
+      (andmap symbol? (second node))))
 
 #|
 
@@ -100,5 +228,36 @@ cases. Help me generate 4 new test cases: 2 positive tests and 2 negative tests.
 ```
 
 ANSWER:
+
+Link to my ChatGPT conversation: https://chatgpt.com/share/68c84a26-5020-8013-bb5b-0e757ea360bf
+
+Here is what ChatGPT gave me for the positive and negative test cases:
+
+------ First Positive Case ------
+(check-true (define-basic? '(define y 42)))
+
+Correct? - Yes
+This test case is valid, the code ```define y 42``` is a correct use of a basic definition.
+
+------ Second Positive Case ------
+(check-true (define-basic? '(define mylist '(1 2 3))))
+
+Correct? - Yes
+This test case is also valid, the code ```define mylist '(1 2 3)``` is a correct way to define a quoted list in racket.
+
+------ First Negative Case ------
+(check-false (define-basic? '(define z)))
+
+Correct? - Yes
+In this test case, there is no value given for the identifier z 
+
+------ Second Negative Case ------
+(check-false (define-basic? '(define "name" 5)))
+
+Correct? - Yes
+Here, the variable name is given as a string literal and not a symbol, which works as a false test case.
+
+Overall I was surprised that ChatGPT was able to understand what programming language was being used without the prompt mentioning racket.
+I was also surprised that it correctly generated 4 working test cases, I tested them with my working code and they all passed properly.
 
 |#
