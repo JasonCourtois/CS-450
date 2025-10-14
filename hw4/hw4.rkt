cdb4ec9382c448375b7552937ca696c0df991fda
JasonCourtois <148589763+JasonCourtois@users.noreply.github.com>
2025-10-14 17:05:57 -0400

    fixed eagerness of problem 1

--- hw4/hw4.rkt
+++ hw4/hw4.rkt
@@ -28,32 +28,35 @@
   )
 )
 (define (stream-skip n s)
-  (: loop 
-    (-> 
-      ;; Real input for the counter.
-      Real
-      ;; Takes an input for stream of elements.
-      (stream Elem)
-      ;; Outputs a stream of elements of the same type as input.
-      (stream Elem)
+  (lambda ()
+    (: loop 
+      (-> 
+        ;; Real input for the counter.
+        Real
+        ;; Takes an input for stream of elements.
+        (stream Elem)
+        ;; Outputs a stream-add of elements of the same type as input.
+        ;; This will eventually get returned as a stream as this loop function is surrounded by a lambda with no arguments. 
+        (stream-add Elem)
+      )
     )
-  )
-  (define (loop counter s)
-    ;; Match the stream to extract the head item from rest of stream.
-    (match (s)
-      [(stream-add h s)
-        (cond
-          ;; If the counter is less than the skip item input n, return the rest of the stream and increment counter.
-          [(< counter n)
-            (loop (+ counter 1) s)]
-          ;; Otherwise, return a stream object by making a lambda with stream add for a return value.
-          [else
-            (lambda () (stream-add h s))])  
-      ]
+    (define (loop counter s)
+      ;; Match the stream to extract the head item from rest of stream.
+      (match (s)
+        [(stream-add h s)
+          (cond
+            ;; If the counter is less than the skip item input n, return the rest of the stream and increment counter.
+            [(< counter n)
+              (loop (+ counter 1) s)]
+            ;; Otherwise, return a stream-add object.
+            [else
+              (stream-add h s)])  
+        ]
+      )
     )
+    ;; Call the loop function with counter initialized to 0.
+    (loop 0 s)
   )
-  ;; Call the loop function with counter initialized to 0.
-  (loop 0 s)
 )
 
 
@@ -86,52 +89,81 @@
   )
 )
 
-; (: set-void set)
-; (define set-void
-;   'todo
-; )
+(: set-void set)
+(define set-void
+  ;; Create a lambda that just returns the empty set when called.
+  (lambda () 
+    (set-empty))
+)
 
-; (: set-epsilon set)
-; (define set-epsilon
-;   'todo
-; )
+(: set-epsilon set)
+(define set-epsilon
+  ;; Lambda returns a set with an empty string followed by empty set.
+  (lambda () 
+    (set-add "" set-void))
+)
 
-; (: set-char (-> Char set))
-; (define (set-char x)
-;   (error "todo")
-; )
+(: set-char (-> Char set))
+(define (set-char x)
+  ;; Lambda converts a character into a string using the string function, then makes a set with it.
+  (lambda () 
+    (set-add (string x) set-void))  
+)
 
 
-; (: set-prefix (-> String set set))
-; (define (set-prefix s p)
-;   (error "todo")
-; )
+(: set-prefix (-> String set set))
+(define (set-prefix s p)
+  (lambda ()
+    ;; Matches set with empty set and non empty set.
+    (match (p)
+      [(set-empty) (set-empty)] ;; Simply return (set-empty) when set is empty.
+      [(set-add h p)
+        ;; Create a new string by appending the prefix to the front of next item.
+        (define newString (string-append s h))
+        ;; Compute the result of applying prefix to the next stream.
+        (define result (set-prefix s p))
+        (set-add newString result)
+      ]
+    )  
+  )
+)
 
-; (: set-union (-> set set set ))
-; (define (set-union p1 p2)
-;   (error "todo")
-; )
+(: set-union (-> set set set ))
+(define (set-union p1 p2)
+  (lambda ()
+    ;; Matches the first set input with empty and non empty cases.
+    (match (p1)
+      [(set-empty) (p2)]  ;; If p1 is empty, return p2
+      [(set-add h p1)
+        ;; result of calling set-union on the p2 and the remainder of p1.
+        (define result (set-union p2 p1))
+        ;; Construct set with head of p1 and the result of recursive call.
+        (set-add h result)
+      ]
+    )
+  )
+)
 
-; (: set-concat (-> set set set))
-; (define (set-concat p1 p2)
-;   (error "todo")
-; )
+(: set-concat (-> set set set))
+(define (set-concat p1 p2)
+  (error "todo")
+)
 
-; (: r:eval-exp (-> r:expression Number))
-; (define (r:eval-exp exp)
-;   (match exp
-;     ; If it's a number, return that number
-;     [(r:number v) v]
-;     ; If it's a function with 2 arguments
-;     [(r:apply (r:variable f) (list arg1 arg2))
-;       (define func (r:eval-builtin f))
-;       (func (r:eval-exp arg1) (r:eval-exp arg2))
-;     ]
-;   )
-; )
+(: r:eval-exp (-> r:expression Number))
+(define (r:eval-exp exp)
+  (match exp
+    ; If it's a number, return that number
+    [(r:number v) v]
+    ; If it's a function with 2 arguments
+    [(r:apply (r:variable f) (list arg1 arg2))
+      (define func (r:eval-builtin f))
+      (func (r:eval-exp arg1) (r:eval-exp arg2))
+    ]
+  )
+)
 
-; (: r:exp-to-string (-> r:expression String))
-; (define (r:exp-to-string exp)
-;   (error "todo")
-; )
+(: r:exp-to-string (-> r:expression String))
+(define (r:exp-to-string exp)
+  (error "todo")
+)
 
