d14ea842dd10c73a88d0afec004f8b65f709a93c
JasonCourtois <148589763+JasonCourtois@users.noreply.github.com>
2025-11-26 12:28:24 -0500

    done lol

--- hw8/hw8.rkt
+++ hw8/hw8.rkt
@@ -21,38 +21,45 @@
 (: env-put (-> handle d:variable d:value (eff-op memory d:void)))
 (define (env-put env var val)
   (lambda ([mem : memory]) : (eff memory d:void)
-    (error "todo")
+    ;; Call environ put to get new memory.
+    (define new-memory (environ-put mem env var val))
+    ;; Return an eff with the new memory and no value.
+    (eff new-memory (d:void))
   )
 )
 
 (: env-push (-> handle d:variable d:value (eff-op memory handle)))
 (define (env-push env var val)
   (lambda ([mem : memory]) : (eff memory handle)
-    (error "todo")
+    (environ-push mem env var val)
   )
 )
 
 (: env-get (-> handle d:variable (eff-op memory d:value)))
 (define (env-get env var)
   (lambda ([mem : memory]) : (eff memory d:value)
-    (error "todo")
+    ;; Get the value from the env
+    (define value (environ-get mem env var))
+    ;; Return the same memory but with the retrieved value
+    (eff mem value)
   )
 )
 
+;; Note from recap lecture: There should be no data structure eff calls in this function.
 (: eval-exp (-> handle d:expression (eff-op memory d:value)))
 (define (eval-exp env exp)
   (match exp
     [(? d:value?)
       ; Return: v
-      (error "todo")
+      (eff-pure exp)
     ]
     [(? d:variable?)
       ; Return: E(x)
-      (error "todo")
+      (env-get env exp)
     ]
     [(d:lambda x e)
       ; Return: {E, λx.t}
-      (error "todo")
+      (eff-pure (d:closure env x e))
     ]
     [(d:apply ef ea)
       (do
@@ -62,12 +69,11 @@
           [(d:closure Ef x tb)
             (do
               ;; ea ⇓E va
-              ; ...
+              va : d:value <- (eval-exp env ea)
               ;; Eb ← Ef + [x := va]
-              ; ...
+              Eb : handle <- (env-push Ef x va)
               ;; tb ⇓Eb vb
-              ; ...
-              (error "todo")
+              (eval-term Eb tb)
             )
           ]
         )
@@ -82,19 +88,17 @@
     [(d:define x e)
       (do
         ;; e ⇓E v
-        ; ...
+        v : d:value <- (eval-exp env e)
         ;; E ← [x := v]
-        ; ...
-        (error "todo")
+        (env-put env x v)
       )
     ]
     [(d:seq t1 t2)
       (do
         ;; ​t1​ ⇓E ​v1
-        ; ...
+        (eval-term env t1)
         ;; t2 ⇓E v2
-        ; ...
-        (error "todo")
+        (eval-term env t2)
       )
     ]
     [(? d:expression?) (eval-exp env term)]
